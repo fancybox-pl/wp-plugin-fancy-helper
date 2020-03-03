8f1d885ab74795e24eeebf6b716a3c114cba8466
Marcin Kosmala <kosmala007@gmail.com>
2020-03-03 14:28:15 +0100

    Remove close button in widget

--- templates/base.html.php
+++ templates/base.html.php
@@ -2,6 +2,5 @@
   <div class="fancy-lifesaver__widget">
     <img src="<?php echo FANCY_LIFESAVER_PLUGIN_URL.'assets/svg/icon.svg' ?>" alt="<?php _e('Help', 'fancy-lifesaver') ?>">
     <span class="fancy-lifesaver__widget-label"><?php _e('Need help?', 'fancy-lifesaver') ?></span>
-    <span class="fancy-lifesaver__widget-close">x</span>
   </div>
 </div>
