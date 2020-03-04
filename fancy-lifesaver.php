e31a0f662035c6327fbe7ef7ccd0a6df78969ff2
Marcin Kosmala <kosmala007@gmail.com>
2020-03-04 14:50:17 +0100

    Check if PHP version is lower than 7 and throw notice if is it

--- fancy-lifesaver.php
+++ fancy-lifesaver.php
@@ -23,12 +23,26 @@ if (!defined('WPINC')) {
     die;
 }
 
-define('FANCY_LIFESAVER_VERSION', '1.0.0');
-define('FANCY_LIFESAVER_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('FANCY_LIFESAVER_PLUGIN_DIR', dirname(__FILE__));
-define('FANCY_LIFESAVER_PLUGIN_BASENAME', plugin_basename(__FILE__));
+if (defined('PHP_MAJOR_VERSION') && PHP_MAJOR_VERSION <= 5) {
+    function fancy_lifesaver_languages()
+    {
+        load_plugin_textdomain('fancy-lifesaver', false, '/fancy-lifesaver/languages/');
+    }
+    add_action('plugins_loaded', 'fancy_lifesaver_languages');
 
-require_once plugin_dir_path(__FILE__).'src/Fancy_Lifesaver.php';
-require_once plugin_dir_path(__FILE__).'src/Controller/Fancy_Lifesaver_Controller.php';
+    function fancy_lifesaver_notice()
+    {
+        echo '<div class="error"><p>'.__('Danger - You are using a very old version of PHP, update it as soon as possible.', 'fancy-lifesaver').'</p></div>';
+    }
+    add_action('admin_notices', 'fancy_lifesaver_notice');
+} else {
+    define('FANCY_LIFESAVER_VERSION', '1.0.0');
+    define('FANCY_LIFESAVER_PLUGIN_URL', plugin_dir_url(__FILE__));
+    define('FANCY_LIFESAVER_PLUGIN_DIR', dirname(__FILE__));
+    define('FANCY_LIFESAVER_PLUGIN_BASENAME', plugin_basename(__FILE__));
 
-$fancy_livesaver = new Fancy_Lifesaver();
+    require_once plugin_dir_path(__FILE__).'src/Fancy_Lifesaver.php';
+    require_once plugin_dir_path(__FILE__).'src/Controller/Fancy_Lifesaver_Controller.php';
+
+    $fancy_livesaver = new Fancy_Lifesaver();
+}
