48968a1e905bfeb4d2638654020c4a5cf6508cdb
Marcin Kosmala <kosmala007@gmail.com>
2020-03-12 09:48:48 +0100

    Send modal form to API and show messages

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,77 @@
-class FancyLivesaver{
-  constructor () {
+class FancyLivesaver {
+  constructor() {
     this.modalElem = document.querySelector('#fancy-lifesaver-modal');
     this.closeBtnElem = document.querySelector('#fancy-lifesaver-close');
     this.showBtnElem = document.querySelector('#fancy-lifesaver-show');
+    this.formElem = document.querySelector('#fancy-lifesaver-form');
+    this.submitElem = document.querySelector('#fancy-lifesaver-submit');
 
-    if (this.closeBtnElem && this.modalElem) {
-      this.closeBtnElem.addEventListener('click', (e) => this.closeModalEventHandler());
-    }
-    if (this.showBtnElem && this.modalElem) {
-      this.showBtnElem.addEventListener('click', (e) => this.showModalEventHandler());
+    if (this.formElem && this.modalElem && this.showBtnElem && this.closeBtnElem && this.submitElem) {
+      this.formElem.reset();
+      this.closeBtnElem.addEventListener('click', () => this.closeModal());
+      this.showBtnElem.addEventListener('click', () => this.showModal());
+      this.formElem.addEventListener('submit', (e) => this.formSubmitEventHandler(e));
     }
   }
 
-  closeModalEventHandler() {
+  closeModal() {
     this.modalElem.style.opacity = '0';
     setTimeout(() => {
       this.modalElem.style.transition = '';
       this.modalElem.style.opacity = '';
       this.modalElem.style.display = 'none';
+      this.formElem.reset();
+      this.submitElem.classList.remove('fancy-lifesaver__modal-submit--sending');
     }, 200);
   }
 
-  showModalEventHandler() {
+  showModal() {
     this.modalElem.style.opacity = '0';
     this.modalElem.style.display = 'block';
     setTimeout(() => {
       this.modalElem.style.opacity = '1';
     }, 200);
+  }
+
+  formSubmitEventHandler(e) {
+    e.preventDefault();
+    const formData = new FormData(this.formElem);
+    this.submitElem.classList.add('fancy-lifesaver__modal-submit--sending');
+
+    fetch('/wp-json/fancy-lifesaver/help', {
+        method: 'post',
+        body: formData,
+      })
+      .then((response) => response.json())
+      .then((response) => {
+        if ('success' == response.status) {
+          this.closeModal();
+          this.notice(response.data);
+        } else {
+          this.closeModal();
+          this.notice(response.data, 'error');
+        }
+      })
+      .catch((error) => console.error('Error:', error));
+  }
+
+  notice(message = '', type = '') {
+    const notice = document.createElement('div');
+    notice.classList.add('fancy-lifesaver-notice');
+    if ('error' == type) {
+      notice.classList.add('fancy-lifesaver-notice--error');
+    }
+    notice.innerHTML = message;
+
+    const button = document.createElement('span');
+    button.addEventListener('click', () => {
+      button.parentNode.remove();
+    })
+    button.classList.add('dismiss');
+
+    notice.appendChild(button);
+
+    document.body.appendChild(notice);
   }
 }
 
--- templates/modal.html.php
+++ templates/modal.html.php
@@ -6,29 +6,37 @@
       <span id="fancy-lifesaver-close" class="fancy-lifesaver__modal-close dashicons dashicons-no-alt"></span>
     </header>
     <div class="fancy-lifesaver__modal-content">
-      <div class="fancy-lifesaver__modal-field required">
-        <label for="fancy-lifesaver-name" class="fancy-lifesaver__modal-label">Imię i nazwisko/nazwa firmy</label>
-        <input id="fancy-lifesaver-name" name="fancy-lifesaver-name" type="text" class="fancy-lifesaver__modal-input">
-      </div>
-      <div class="fancy-lifesaver__modal-field required">
-        <label for="fancy-lifesaver-email" class="fancy-lifesaver__modal-label">Adres email do kontaktu</label>
-        <input id="fancy-lifesaver-email" name="fancy-lifesaver-email" type="email" class="fancy-lifesaver__modal-input">
-      </div>
-      <div class="fancy-lifesaver__modal-field">
-        <label for="fancy-lifesaver-phone" class="fancy-lifesaver__modal-label">Numer telefonu</label>
-        <input id="fancy-lifesaver-phone" name="fancy-lifesaver-phone" type="text" class="fancy-lifesaver__modal-input">
-      </div>
-      <div class="fancy-lifesaver__modal-field required">
-        <label for="fancy-lifesaver-content" class="fancy-lifesaver__modal-label">Opis problemu</label>
-        <textarea id="fancy-lifesaver-content" name="fancy-lifesaver-content" class="fancy-lifesaver__modal-textarea"></textarea>
-      </div>
-      <div class="fancy-lifesaver__modal-field required">
-        <input id="fancy-lifesaver-acceptance" name="fancy-lifesaver-acceptance" type="checkbox" class="fancy-lifesaver__modal-input-checkbox">
-        <label for="fancy-lifesaver-acceptance" class="fancy-lifesaver__modal-label-checkbox">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tollenda est atque extrahenda radicitus.</label>
-      </div>
-      <div class="fancy-lifesaver__modal-field" style="text-align: center">
-        <button class="fancy-lifesaver__modal-submit"><span class="dashicons dashicons-yes"></span> Wyślij</button>
-      </div>
+      <form id="fancy-lifesaver-form">
+        <div class="fancy-lifesaver__modal-field required">
+          <label for="fancy-lifesaver-name" class="fancy-lifesaver__modal-label">Imię i nazwisko/nazwa firmy</label>
+          <input id="fancy-lifesaver-name" name="fancy-lifesaver-name" type="text" class="fancy-lifesaver__modal-input" required>
+        </div>
+        <div class="fancy-lifesaver__modal-field required">
+          <label for="fancy-lifesaver-email" class="fancy-lifesaver__modal-label">Adres email do kontaktu</label>
+          <input id="fancy-lifesaver-email" name="fancy-lifesaver-email" type="email" class="fancy-lifesaver__modal-input" required>
+        </div>
+        <div class="fancy-lifesaver__modal-field">
+          <label for="fancy-lifesaver-phone" class="fancy-lifesaver__modal-label">Numer telefonu</label>
+          <input id="fancy-lifesaver-phone" name="fancy-lifesaver-phone" type="text" class="fancy-lifesaver__modal-input">
+        </div>
+        <div class="fancy-lifesaver__modal-field required">
+          <label for="fancy-lifesaver-content" class="fancy-lifesaver__modal-label">Opis problemu</label>
+          <textarea id="fancy-lifesaver-content" name="fancy-lifesaver-content" class="fancy-lifesaver__modal-textarea" required></textarea>
+        </div>
+        <div class="fancy-lifesaver__modal-field required">
+          <input id="fancy-lifesaver-acceptance" name="fancy-lifesaver-acceptance" type="checkbox" class="fancy-lifesaver__modal-input-checkbox" required>
+          <label for="fancy-lifesaver-acceptance" class="fancy-lifesaver__modal-label-checkbox">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tollenda est atque extrahenda radicitus.</label>
+        </div>
+        <div class="fancy-lifesaver__modal-field" style="text-align: center">
+          <button id="fancy-lifesaver-submit" class="fancy-lifesaver__modal-submit">
+            <span class="fancy-lifesaver__modal-icons">
+              <span class="dashicons dashicons-yes"></span>
+              <span class="dashicons dashicons-image-rotate"></span>
+            </span>
+            Wyślij
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
