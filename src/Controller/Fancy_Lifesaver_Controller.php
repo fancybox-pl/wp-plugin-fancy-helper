c903fd0eb069e2025d5ca69cd1c21a35270aea8e
Marcin Kosmala <kosmala007@gmail.com>
2020-03-12 12:08:32 +0100

    Restrict API route for admin, send email with notify

--- src/Controller/Fancy_Lifesaver_Controller.php
+++ src/Controller/Fancy_Lifesaver_Controller.php
@@ -8,12 +8,31 @@ class Fancy_Lifesaver_Controller
         add_action('admin_footer', [$this, 'modal_template']);
     }
 
-    public function send_help_message()
+    public function send_help_message(\WP_REST_Request $request)
     {
-        return [
+        $response = [
             'status' => 'success',
-            'data' => 'Lorem ipsum',
+            'data' => __('Wiadomość została wysłana'),
         ];
+
+        $form_data = $request->get_params();
+        $subject = 'Fancy Lifesaver - new issue';
+        $headers = ['Content-Type: text/html; charset=UTF-8'];
+        $body = '
+            <p><b>'.__('Imię i nazwisko/nazwa firmy').'</b>: '.$form_data['fancy-lifesaver-name'].'</p>
+            <p><b>'.__('E-mail').'</b>: '.$form_data['fancy-lifesaver-email'].'</p>
+            <p><b>'.__('Telefon').'</b>: '.$form_data['fancy-lifesaver-phone'].'</p>
+            <p><b>'.__('Opis problemu').'</b></p>
+            '.$form_data['fancy-lifesaver-content'].'
+        ';
+
+        $result = wp_mail('m.kosmala@fancybox.pl', $subject, $body, $headers);
+        if (!$result) {
+            $response['status'] = 'fail';
+            $response['status'] = __('Błąd podczas wysyłania wiadomości');
+        }
+
+        return $response;
     }
 
     public function widget_template()
--- src/routing.php
+++ src/routing.php
@@ -1,8 +1,12 @@
 <?php
 
 add_action('rest_api_init', function () {
-    register_rest_route('fancy-lifesaver', '/help', [
-        'methods' => 'POST',
-        'callback' => ['Fancy_Lifesaver_Controller', 'send_help_message'],
-    ]);
+    if (current_user_can('editor') || current_user_can('administrator')) {
+
+        register_rest_route('fancy-lifesaver', '/help', [
+            'methods' => 'POST',
+            'callback' => ['Fancy_Lifesaver_Controller', 'send_help_message'],
+        ]);
+
+    }
 });
